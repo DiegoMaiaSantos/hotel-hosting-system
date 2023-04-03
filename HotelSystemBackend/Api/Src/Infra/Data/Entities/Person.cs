0c272777d54731d3937da9d5f91dfd47662f23bc
Diego Santos <diegom.santos03@gmail.com>
2023-04-02 21:04:54 -0300

    (chore): created entities

---
+++ HotelSystemBackend/Api/Src/Infra/Data/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Src.Infra.Data.Entities
+{
+    public class Person : Reserve
+    {
+        public int IdGuest { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int IdHostedSuite { get; set; }
+        public Suite Suite { get; set; }
+        public Reserve Reserve { get; set; }
+    }
+}
