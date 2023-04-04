a28bc2732ff06e08eadf96f24bd272dbf52ebb89
Diego Santos <diegom.santos03@gmail.com>
2023-04-03 23:11:42 -0300

    (chore): created interface of unitofwork and the class

---
+++ HotelSystemBackend/Api/Src/Infra/Data/Contexts/Interface/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Src.Infra.Data.Contexts.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task BeginTransaction();
+        Task Commit();
+        Task Rollback();
+    }
+}
---
+++ HotelSystemBackend/Api/Src/Infra/Data/Contexts/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Api.Src.Infra.Data.Contexts.Interface;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Api.Src.Infra.Data.Contexts
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HotelSystemDbContext _hotelSystemDbContext;
+        private IDbContextTransaction _transaction;
+
+        public UnitOfWork(HotelSystemDbContext hotelSystemDbContext)
+        {
+            _hotelSystemDbContext = hotelSystemDbContext;
+        }
+        public async Task BeginTransaction()
+        {
+            _transaction = await _hotelSystemDbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task Commit()
+        {
+            await _transaction.CommitAsync();
+        }
+
+        public async Task Rollback()
+        {
+            await _transaction.RollbackAsync();
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
