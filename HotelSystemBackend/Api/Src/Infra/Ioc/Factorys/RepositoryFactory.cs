fdd2c2cd822cebf7a8292efa61c7e04524bbedcb
Diego Santos <diegom.santos03@gmail.com>
2023-04-03 23:14:05 -0300

    (chore): created RepositoryFactory and ServiceFactory

---
+++ HotelSystemBackend/Api/Src/Infra/Ioc/Factorys/RepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using Api.Src.Domain.Interfaces.Repositories;
+using Api.Src.Infra.Data.Contexts.Interface;
+using Api.Src.Infra.Data.Repositories;
+
+namespace Api.Src.Infra.Ioc.Factorys
+{
+    public static class RepositoryFactory
+    {
+        public static IServiceCollection RegisterRepositories(this IServiceCollection repositories)
+        {
+            repositories.AddScoped<IUnitOfWork, IUnitOfWork>();
+            repositories.AddScoped<IPersonHotelSystemRepository, PersonHotelSystemRepository>();
+            repositories.AddScoped<IReserveHotelSystemRepository, ReserveHotelSystemRepository>();
+            repositories.AddScoped<ISuiteHotelSystemRepository, SuiteHotelSystemRepository>();
+
+            return repositories;
+        }
+    }
+}
---
+++ HotelSystemBackend/Api/Src/Infra/Ioc/Factorys/ServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using Api.Src.Domain.Interfaces.Services;
+using Api.Src.Services;
+
+namespace Api.Src.Infra.Ioc.Factorys
+{
+    public static class ServiceFactory
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPersonHotelSystemService, PersonHotelSystemService>();
+            services.AddScoped<IReserveHotelSystemService, ReserveHotelSystemService>();
+            services.AddScoped<ISuiteHotelSystemService, SuiteHotelSystemService>();
+
+            return services;
+        }
+    }
+}
