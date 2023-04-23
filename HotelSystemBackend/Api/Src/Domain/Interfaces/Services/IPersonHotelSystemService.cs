933010758cbc9f510fd55b9adf2ee439f0f7148e
Diego Santos <diegom.santos03@gmail.com>
2023-04-23 10:27:43 -0300

    (chore): created person interface

--- HotelSystemBackend/Api/Src/Domain/Interfaces/Services/IPersonHotelSystemService.cs
+++ HotelSystemBackend/Api/Src/Domain/Interfaces/Services/IPersonHotelSystemService.cs
@@ -1,8 +1,14 @@
 ﻿using Api.Src.Infra.Data.Entities;
+using System;
 
 namespace Api.Src.Domain.Interfaces.Services
 {
     public interface IPersonHotelSystemService
     {
+        Task<List<Person>> GetAll();
+        Task<Person> GetByName(Person namePerson);
+        Task<Person> NewPerson(Person person);
+        Task<Person> ChangePerson(Person person);
+        Task<Person> DeletePerson(Person namePerson);
     }
 }
