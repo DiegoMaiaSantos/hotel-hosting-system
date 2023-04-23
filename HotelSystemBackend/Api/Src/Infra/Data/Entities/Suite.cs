142601f4d1dd03cbdeac81a4f53b1e696eae9ada
Diego Santos <diegom.santos03@gmail.com>
2023-04-23 10:29:27 -0300

    (refactor): changes and improvement in entities

--- HotelSystemBackend/Api/Src/Infra/Data/Entities/Suite.cs
+++ HotelSystemBackend/Api/Src/Infra/Data/Entities/Suite.cs
@@ -1,13 +1,13 @@
 ﻿namespace Api.Src.Infra.Data.Entities
 {
-    public class Suite : Reserve
+    public class Suite
     {
-        public int IdSuite { get; set; }
-        public string SuiteType { get; set; }
+        public int SuiteId { get; set; }
+        public string? SuiteType { get; set; }
         public int Capacity { get; set; }
         public decimal DailyValue { get; set; }
-        public int IdPersonSuite { get; set; }
-        public Person Person { get; set; }
-        public Reserve Reserve { get; set; }
+        public int PersonSuiteId { get; set; }
+        public virtual Person? Person { get; set; }
+        public virtual Reserve? Reserve { get; set; }
     }
 }
