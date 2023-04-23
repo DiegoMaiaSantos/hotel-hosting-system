98c1fba03ebeed22c561704a98518375319337c5
Diego Santos <diegom.santos03@gmail.com>
2023-04-23 10:33:25 -0300

    (chore): started mapping the entities that carry out part of the communication with the database

--- HotelSystemBackend/Api/Src/Infra/Data/Mappers/PersonMapper.cs
+++ HotelSystemBackend/Api/Src/Infra/Data/Mappers/PersonMapper.cs
@@ -1,9 +1,25 @@
 ﻿using Api.Src.Infra.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Api.Src.Infra.Data.Mappers
 {
     public class PersonMapper : IEntityTypeConfiguration<Person>
     {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Person");
+
+            builder.HasKey(e => e.GuestId);
+
+            builder.HasMany(e => e.Suite)
+                .WithOne(e => e.Person)
+                .HasForeignKey(e => e.HostedSuiteId);
+
+            builder.HasMany(e => e.Reserve)
+                .WithOne(e => e.Person)
+                .HasForeignKey(e => e.HostedSuiteId);
+
+        }
     }
 }
