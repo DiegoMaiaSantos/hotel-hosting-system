69b2c124ad7ac6e475202b1525e96224d9ee047e
Diego Santos <diegom.santos03@gmail.com>
2023-09-25 21:52:53 -0300

    (chore): created entities and changed existing ones

---
+++ HotelSystemBackend/Api/Src/Infra/Data/Entities/ServiceOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Src.Infra.Data.Entities
+{
+    public class ServiceOrder : BaseEntity
+    {
+        public int ServiceOrderId { get; set; }
+        public int SuiteId { get; set; }
+        public int TotalDays { get; set; }
+        public string ExtraCleaning { get; set; }
+        public int EmployeeId { get; set; }
+        public decimal ValueTotal { get; set; }
+    }
+}
